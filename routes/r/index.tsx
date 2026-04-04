848310c0c94cee597823071ba9ad3c61db676871
kamekyame <kamekyame@outlook.com>
2026-04-05 02:25:11 +0900

    👍 テスト用にページのexport方式を変更

--- routes/r/index.tsx
+++ routes/r/index.tsx
@@ -3,30 +3,32 @@ import { createCruxVisUrl } from "#/src/cruxvis.ts";
 import { createFeedTitle, parseQueryParams } from "#/src/utils.ts";
 import { define } from "#/utils.ts";
 
-export default define.page((c) => {
-  const reqUrl = new URL(c.req.url);
-  const reqQueryParams = parseQueryParams(reqUrl.searchParams);
-  const asof = reqUrl.searchParams.get("asof");
+export const handler = define.handlers({
+  GET(c) {
+    const reqUrl = new URL(c.req.url);
+    const reqQueryParams = parseQueryParams(reqUrl.searchParams);
+    const asof = reqUrl.searchParams.get("asof");
 
-  const title = `${createFeedTitle(reqQueryParams)} as of ${asof}`;
-  const ogImageUrl = `${reqUrl.origin}/i?${reqUrl.searchParams.toString()}`;
-  const cruxVisUrl = createCruxVisUrl(reqQueryParams);
+    const title = `${createFeedTitle(reqQueryParams)} as of ${asof}`;
+    const ogImageUrl = `${reqUrl.origin}/i?${reqUrl.searchParams.toString()}`;
+    const cruxVisUrl = createCruxVisUrl(reqQueryParams);
 
-  return (
-    <html>
-      <head prefix="og: http://ogp.me/ns#">
-        <title>{title}</title>
-        <meta property="og:url" content={c.req.url} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:site_name" content="CrUX Feed" />
-        <meta property="og:image" content={ogImageUrl} />
-        <meta name="robots" content="noindex, noffollow" />
-      </head>
-      <body>
-        <h1>{title}</h1>
-        <RedirectCrUXVis redirectUrl={cruxVisUrl} />
-      </body>
-    </html>
-  );
+    return c.render(
+      <html>
+        <head prefix="og: http://ogp.me/ns#">
+          <title>{title}</title>
+          <meta property="og:url" content={c.req.url} />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={title} />
+          <meta property="og:site_name" content="CrUX Feed" />
+          <meta property="og:image" content={ogImageUrl} />
+          <meta name="robots" content="noindex, noffollow" />
+        </head>
+        <body>
+          <h1>{title}</h1>
+          <RedirectCrUXVis redirectUrl={cruxVisUrl} />
+        </body>
+      </html>,
+    );
+  },
 });
