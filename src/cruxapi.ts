ea4e558b9372c211857ae9ff4a49f9cfc852d538
kamekyame <kamekyame@outlook.com>
2026-04-05 23:08:05 +0900

    ♻️ fetch関数を渡してglobalThisへの代入を削減

--- src/cruxapi.ts
+++ src/cruxapi.ts
@@ -1,8 +1,6 @@
 import { createQueryHistoryRecord } from "crux-api";
 
-// deno-lint-ignore no-explicit-any
-globalThis.window = globalThis as any;
-
 export const queryHistoryRecord = createQueryHistoryRecord({
   key: Deno.env.get("GOOGLE_API_KEY") as string,
+  fetch: globalThis.fetch,
 });
